87a085ecb4ec68081e536c1dbc5fbf0f1cba140b
mcontoor <mcontoor@gmail.com>
2019-07-27 22:44:20 +0530

    connect database

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(
@@ -7,6 +8,11 @@ app.use(
     extended: true
   })
 );
+
+mongoose
+  .connect("mongodb://localhost:27017/pizzedeals", { useNewUrlParser: true })
+  .then(res => console.log("MongoDB connected"))
+  .catch(err => console.log("Err at connection with mongo ", err));
 
 const PORT = 5000;
 
